10eb36aaf17172e86f716dc05c9090542c3be38a
Sudhir Yadav <sudhiry875@gmail.com>
2017-07-29 22:35:03 +0530

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Twitter</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+hello  asdas
+</body>
+</html>
