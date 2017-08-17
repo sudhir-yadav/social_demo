f75ac27896393bfd4d1abb29f6e31d0586b1ba01
Sudhir Yadav <sudhiry875@gmail.com>
2017-08-18 00:37:39 +0530

    minor

--- authenticate.php
+++ authenticate.php
@@ -92,7 +92,7 @@ if ($result->num_rows == 1) {
         $i++;
     }
 } else {
-    $sql1 = "INSERT INTO user_master (name, email, fb_id) VALUES ('".$user['id']."','".$user['name']."','".$user['email']."','".$user['id']."')";
+    $sql1 = "INSERT INTO user_master (id,name, email, fb_id) VALUES ('".$user['id']."','".$user['name']."','".$user['email']."','".$user['id']."')";
     $_SESSION['user']['id'] = $user['id'];
     $conn->query($sql1);
     // $_SESSION['user']['id'] = mysql_insert_id();
